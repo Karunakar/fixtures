5f52edc3384122701511866317f4e675a97b5063
Karunakar (Ruby) <revurikarna@gmail.com>
2011-01-16 23:13:25 +0530

    added basic capistrano scripts for localhost and ec2 image samples

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,29 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "fixtures"
+set :repository,  "git@github.com:Karunakar/fixtures.git"
+set :deploy_via, :remote_cache
+set :scm, :git
+set :application, "fixtures"
+set :user, 'jyothi'
+#set :deploy_via, :remote_cache
 
-set :scm, :subversion
+set :use_sudo, false
+default_run_options[:pty] = true
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :deploy_to, '/u/apps/fixtures'
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+
+role :web, "127.0.0.1"                          # Your HTTP server, Apache/etc
+role :app, "127.0.0.1"                          # This may be the same as your `Web` server
+role :db,  "127.0.0.1", :primary => true # This is where Rails migrations will run
 
 # If you are using Passenger mod_rails uncomment this:
 # if you're still using the script/reapear helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+ namespace :deploy do
+   task :start do ; end
+   task :stop do ; end
+   task :restart, :roles => :app, :except => { :no_release => true } do
+     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
+ end
